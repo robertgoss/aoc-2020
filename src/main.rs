e77886d5f0d83a3d522e1d60ddb895acc254b350
Robert Goss <goss.robert@gmail.com>
2020-12-01 17:07:26 +0000

    Do day 1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
 
+mod expenses {
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
+
+
+    // find pair of numbers in values list that sum together to make total
+    fn _find_2_summands(value_set : &HashSet<i64>, total : i64) -> Option<(i64, i64)> {
+        value_set.iter().filter(
+            |&value| value_set.contains(&(total - value))
+        ).next().map(
+            |&summand| (summand, total - summand)
+        )
+    }
+
+    // find triple of numbers in values list that sum together to make total
+    fn _find_3_summands(value_set : &HashSet<i64>, total : i64) -> Option<(i64, i64, i64)> {
+        value_set.iter().filter_map(
+            |&value| _find_2_summands(value_set, total - value)
+        ).next().map(
+            |(a, b)| (a, b, total - a - b)
+        )
+    }
+
+    // find pair of numbers in values list that sum together to make total
+    pub fn find_2_summands(values : &Vec<i64>, total : i64) -> Option<(i64, i64)> {
+        // Query set to avoid quadratic check
+        let value_set : HashSet<i64> = 
+          HashSet::from_iter(values.iter().cloned());
+        _find_2_summands(&value_set, total)
+    }
+
+    // find triple of numbers in values list that sum together to make total
+    pub fn find_3_summands(values : &Vec<i64>, total : i64) -> Option<(i64, i64, i64)> {
+        // Query set to avoid quadratic check
+        let value_set : HashSet<i64> = 
+          HashSet::from_iter(values.iter().cloned());
+        _find_3_summands(&value_set, total)
+    }
+}
+
 mod io {
-    use std::io::Read;
     use std::io::BufRead;
     use std::fs::File;
     use std::io::BufReader;
 
-    fn input_as_list(day: i8) -> Vec<i64> {
-        let filename = format!("data\\day-{}.txt", day);
+    pub fn input_as_list(day: i8) -> Vec<i64> {
+        let filename = format!("data/day-{}.txt", day);
         let file = File::open(filename).expect("Issue opening file");
         let reader = BufReader::new(&file);
         reader.lines().map(
@@ -17,15 +56,24 @@ mod io {
 
 mod challenge {
     use super::io as io;
+    use super::expenses as expenses;
 
     fn challenge_1() {
         let data = io::input_as_list(1);
-        println!("{}", &data);
+        let (a, b) = expenses::find_2_summands(&data, 2020).unwrap();
+        println!("{} {} {}", a , b, a * b);
+    }
+
+    fn challenge_2() {
+        let data = io::input_as_list(1);
+        let (a, b, c) = expenses::find_3_summands(&data, 2020).unwrap();
+        println!("{} {} {} {}", a, b, c, a * b * c);
     }
 
     pub fn challenge(num : u8) {
         match num {
             1 => challenge_1(),
+            2 => challenge_2(),
             _ => () 
         }
     }
@@ -34,5 +82,5 @@ mod challenge {
 
 
 fn main() {
-    challenge::challenge(1);
+    challenge::challenge(2);
 }
